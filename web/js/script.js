e68e4601fec401ca4439b4a55a260399bfb68089
RodrigoBatista27 <102333705+RodrigoBatista27@users.noreply.github.com>
2023-05-19 21:45:38 -0300

    Delete web directory

--- web/js/script.js
+++
@@ -1,7 +0,0 @@
-const toggleMenu = document.getElementById('toggle-menu');
-const menu = document.querySelector('.menu');
-
-toggleMenu.addEventListener('click', () => {
-  toggleMenu.classList.toggle('opened');
-  menu.classList.toggle('opened');
-});
